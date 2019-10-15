d2802d2242b27089fe2566096a6c971d21c9ac62
TokenJan <jiahuan.yang@outlook.com>
2019-10-15 17:07:36 +0800

    add webcam environment

---
+++ ui.js
@@ -0,0 +1,95 @@
+/**
+ * @license
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+import * as tf from '@tensorflow/tfjs';
+
+const CONTROLS = ['up', 'down', 'left', 'right'];
+const CONTROL_CODES = [38, 40, 37, 39];
+
+export function init() {
+  document.getElementById('controller').style.display = '';
+  statusElement.style.display = 'none';
+}
+
+const trainStatusElement = document.getElementById('train-status');
+
+// Set hyper params from UI values.
+const learningRateElement = document.getElementById('learningRate');
+export const getLearningRate = () => +learningRateElement.value;
+
+const batchSizeFractionElement = document.getElementById('batchSizeFraction');
+export const getBatchSizeFraction = () => +batchSizeFractionElement.value;
+
+const epochsElement = document.getElementById('epochs');
+export const getEpochs = () => +epochsElement.value;
+
+const denseUnitsElement = document.getElementById('dense-units');
+export const getDenseUnits = () => +denseUnitsElement.value;
+const statusElement = document.getElementById('status');
+
+export function startPacman() {
+  // google.pacman.startGameplay();
+}
+
+export function predictClass(classId) {
+  // google.pacman.keyPressed(CONTROL_CODES[classId]);
+  document.body.setAttribute('data-active', CONTROLS[classId]);
+}
+
+export function isPredicting() {
+  statusElement.style.visibility = 'visible';
+}
+
+export function donePredicting() {
+  statusElement.style.visibility = 'hidden';
+}
+
+export let addExampleHandler;
+export function setExampleHandler(handler) {
+  addExampleHandler = handler;
+}
+let mouseDown = false;
+const totals = [0, 0, 0, 0];
+
+const upButton = document.getElementById('up');
+const downButton = document.getElementById('down');
+const leftButton = document.getElementById('left');
+const rightButton = document.getElementById('right');
+
+async function handler(label) {
+  mouseDown = true;
+  const className = CONTROLS[label];
+  const total = document.getElementById(className + '-total');
+  while (mouseDown) {
+    addExampleHandler(label);
+    document.body.setAttribute('data-active', CONTROLS[label]);
+    total.innerText = ++totals[label];
+    await tf.nextFrame();
+  }
+  document.body.removeAttribute('data-active');
+}
+
+upButton.addEventListener('mousedown', () => handler(0));
+upButton.addEventListener('mouseup', () => mouseDown = false);
+
+downButton.addEventListener('mousedown', () => handler(1));
+downButton.addEventListener('mouseup', () => mouseDown = false);
+
+leftButton.addEventListener('mousedown', () => handler(2));
+leftButton.addEventListener('mouseup', () => mouseDown = false);
+
+rightButton.addEventListener('mousedown', () => handler(3));
+rightButton.addEventListener('mouseup', () => mouseDown = false);
