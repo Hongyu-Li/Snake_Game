0d313f7cba1f442312f4b0ba3c22be4c9f237ae1
hongyu.li <hongyu.li@thoughtworks.com>
2019-10-23 16:10:04 +0800

    [Hongyu] feat: train a smaller headmodel

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -94,7 +94,7 @@ export default class Panel extends React.Component<GameProps> {
     async loadAddonModel() {
       console.log('Loading model......');
       try {
-        const model = await tf.loadLayersModel("./tfjs_model/model.json");
+        const model = await tf.loadLayersModel("./tfjs_model_smaller/model.json");
         document.getElementById("status").style.display = "none";
         document.getElementById("main").style.display = "block";
         console.log('Done loading pretrained model.');
